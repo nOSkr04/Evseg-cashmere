eefcd127d2753b2115ce62820c9b5e23e621fd35
nOSkr <118327643+nOSkr@users.noreply.github.com>
2023-12-28 17:50:32 +0800

    sync

--- src/models/user.tsx
+++ src/models/user.tsx
@@ -1,14 +1,25 @@
 import { IUser } from "../interface/user";
 
+enum UserType {
+  "admin",
+  "operator",
+  "driver"
+}
 export class User implements IUser {
   _id: string;
   firstName: string;
   lastName: string;
+  phone: string;
+  nationalId: string;
+  userType: "admin" | "operator" | "driver" | "user"
 
-  constructor({ _id, firstName, lastName }: IUser) {
+  constructor({ _id, firstName, lastName, phone, nationalId,userType }: IUser) {
     this._id = _id;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.phone = phone;
+    this.nationalId = nationalId;
+    this.userType = userType;
   }
 
   static fromJson(json: IUser) {
